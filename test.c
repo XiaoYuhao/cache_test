2910cb9780328ce5af825f33e3b8243266734f8c
root <752087942@qq.com>
2020-09-27 22:44:46 +0800

    update

--- test.c
+++ test.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
 
 int main(){
+    srand(time(NULL));
     clock_t start, end;
-    int total = 200000000;
     start = clock();
     printf("start time: %ld\n", start);
-    total = 1000000000;
-    long a = 0;
-    for(int i=0;i<total;i++){
-        a+=i;
+    //
+    int a=0, b=0, total = 2000;
+    int k = 600;
+    int size = sizeof(int)*k*1024;
+    printf("Memory Size: %d\n", size);
+    int *nums = (int *)malloc(size);
+    while(total--){
+        /*for(int i=0;i<k*1024;i++){
+            b = rand()%(k*1024);
+            a = nums[i];
+        }*/
+        for(int i=0;i<100000;i++){
+            a += nums[rand()%(k*1024)];
+            //b = a;
+        }
     }
+    //
     end = clock();
     printf("end time: %ld\n", end);
     double cost = (double)(end - start)/CLOCKS_PER_SEC;
