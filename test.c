eee53d09f3067e6512819447ea81a19b099bc8c0
root <752087942@qq.com>
2020-09-27 19:44:10 +0800

    update

--- test.c
+++ test.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#include<time.h>
 
 int main(){
-    printf("%d\n",sizeof(int));
+    clock_t start, end;
+    start = clock();
+    printf("start time: %ld\n", start);
+    int total = 20000000;
+    for(int i=0;i<total;i++){
+        //total--;
+    }
+    end = clock();
+    printf("end time: %ld\n", end);
+    long cost = end - start;
+    printf("cost time: %ld\n",cost);
     return 0;
 }
