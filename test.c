992590995542ce023f88382d41a7c98ad7262d37
root <752087942@qq.com>
2020-09-30 16:41:44 +0800

    update

--- test.c
+++ test.c
@@ -1,32 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
+
+int nums[1024*1024];
+
+int test(int k){
+    clock_t start, end;
+    start = clock();
+    long sum = 0;
+    int size = sizeof(int)*k*1024;
+    printf("Memory Size: %d ====> ", size);
+
+    //下面是顺序读取
+    for(int i=0;i<100000000;i++){
+        rand();
+        sum += nums[i%(k*1024)];
+    }
+    /*
+    //下面是随机读取
+    for(int i=0;i<100000000;i++){
+        sum += nums[rand()%(k*1024)];
+    }
+    */
+    
+    end = clock();
+    double cost = (double)(end - start)/CLOCKS_PER_SEC;
+    printf("cost time: %lf\n",cost);
+    return sum;
+}
 
 int main(){
     srand(time(NULL));
-    clock_t start, end;
-    start = clock();
-    printf("start time: %ld\n", start);
-    //
-    int a=0, b=0, total = 2000;
-    int k = 600;
-    int size = sizeof(int)*k*1024;
-    printf("Memory Size: %d\n", size);
-    int *nums = (int *)malloc(size);
-    while(total--){
-        /*for(int i=0;i<k*1024;i++){
-            b = rand()%(k*1024);
-            a = nums[i];
-        }*/
-        for(int i=0;i<100000;i++){
-            a += nums[rand()%(k*1024)];
-            //b = a;
-        }
+    memset(nums, -1, sizeof(nums));
+
+    for(int i=16;i<=1024;i+=16){
+        test(i);
     }
-    //
-    end = clock();
-    printf("end time: %ld\n", end);
-    double cost = (double)(end - start)/CLOCKS_PER_SEC;
-    printf("cost time: %lf\n",cost);
+    
     return 0;
 }
